17a7b5fff9fffd856b8ffa445a15280a801999b6
unknown <vishal.makwana3@gmail.com>
2019-02-10 13:14:53 +0530

    Yash thru Vishal

--- App.js
+++ App.js
@@ -1,39 +1,23 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator,createAppContainer} from 'react-navigation';
-import Welcome from './src/Screens/Welcome';
-import  Chat from './src/Screens/Chat';
-import Home from './src/Screens/Home';
-import Constants from './src/Constants';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Welcome from "./src/Screens/Welcome";
+import Chat from "./src/Screens/Chat";
+import Home from "./src/Screens/Home";
+import Constants from "./src/Constants";
 
-
-const AppNavigator = createStackNavigator({
-  Welcome:Welcome,
-  Home:Home,
-  Chat:Chat
-},{
-  initialRouteName:'Welcome'
-},
-{
-  navigationOptions:{
-    headerStyle: {
-      backgroundColor: Constants.SECONDARY,
-      borderBottomWidth: 0,
-      borderRadius: 0.5,
-      elevation: 0,
-    },
-    headerBackTitle: null,
-    headerTintColor: Constants.PRIMARY,
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
+const AppNavigator = createStackNavigator(
+  {
+    Welcome: Welcome,
+    Home: Home,
+    Chat: Chat
+  },
+  {
+    initialRouteName: "Home"
   }
-}
 );
 
-
 export default class App extends React.Component {
-
   static navigationOptions = ({ navigation }) => {
     header: false;
   };
@@ -42,5 +26,3 @@ export default class App extends React.Component {
     return <AppNavigator />;
   }
 }
-
-
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,35 +1,129 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import React from "react";
+
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  Dimensions
+} from "react-native";
+import Constants from "../Constants";
 
 const styles = StyleSheet.create({
-    mainStyle: {
-        flex: 1,
-        justifyContent: 'center',
-    
-    }
+  mainStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
 
-export default class Home extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      event_key: "",
+      event_id: ""
+    };
+  }
 
-        }
-    }
+  static navigationOptions = ({ navigation }) => {
+    return {
+      header: null
+    };
+  };
 
-    componentDidMount(){
+  componentDidMount() {
+    StatusBar.setBackgroundColor("rgba(245,245,245,1)");
+    StatusBar.setBarStyle("dark-content", true);
+  }
 
-    }
+  componentWillUnmount() {}
 
-    componentWillUnmount(){
-
-    }
-
-    render(){
-        return(
-            <View style={styles.mainStyle}> 
-            <Text>Home</Text>               
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.mainStyle}>
+        <View
+          style={{
+            width: Dimensions.get("window").width,
+            opacity: 0.5,
+            paddingBottom: 50,
+            paddingHorizontal: 20,
+            alignItems: "center"
+          }}
+        >
+          <View style={{ height: Dimensions.get("screen").height / 2.5 }} />
+          <Text
+            style={{
+              fontSize: 25,
+              alignSelf: "flex-start",
+              fontWeight: "600"
+            }}
+          >
+            Welcome Back,
+          </Text>
+          <Text style={{ fontSize: 20, alignSelf: "flex-start" }}>
+            Please enter
+          </Text>
+          <Text style={{ fontSize: 20, alignSelf: "flex-start" }}>
+            credentials
+          </Text>
+        </View>
+        <View
+          style={{
+            elevation: 1,
+            borderRadius: 5,
+            width: Dimensions.get("screen").width / 1.1
+          }}
+        >
+          <TextInput
+            keyboardType="email-address"
+            style={{
+              paddingVertical: 10,
+              paddingHorizontal: 20
+            }}
+            placeholder="Event Key"
+          />
+          <View
+            style={{
+              width: "100%",
+              height: 1,
+              backgroundColor: Constants.LIGHTGREY
+            }}
+          />
+          <TextInput
+            secureTextEntry={true}
+            style={{
+              paddingVertical: 10,
+              paddingHorizontal: 20
+            }}
+            placeholder="Event ID"
+          />
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{
+            width: Dimensions.get("screen").width / 1.05,
+            borderRadius: 10,
+            height: 50,
+            backgroundColor: Constants.SECONDARY,
+            marginVertical: 10,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: Constants.PRIMARY
+            }}
+          >
+            Connect
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
